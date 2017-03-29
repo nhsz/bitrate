906138655c2001ccbda96a44eeafc8c53a8017f0
Nicolás Quiroz <nh.quiroz@gmail.com>
2017-03-29 20:54:19 -0300

    Source added

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
-function main () {
-  var urlBitfinex = 'https://api.bitfinex.com/v2/ticker/tBTCUSD';
+(function main () {
+  var url = 'https://api.bitfinex.com/v2/ticker/tBTCUSD';
   var lastRate = 0;
-  var updateFrequencyInMs = 30000; // the exchange rate is updated every 30 seconds by default
+  var updateFrequencyInMs = 60000; // the exchange rate is updated every 60s
 
   var currentRateReq = new XMLHttpRequest();
   currentRateReq.onload = getRate;
   currentRateReq.onerror = reqError;
-  currentRateReq.open('get', urlBitfinex, true);
+  currentRateReq.open('get', url, true);
   currentRateReq.send();
 
-  function getRate() {
+  function getRate () {
     lastRate = JSON.parse(this.responseText)[6];
     showRates(lastRate, lastRate);
   }
 
-  setInterval(function() {
-    function reqListener() {
+  setInterval(function () {
+    function reqListener () {
       var data = JSON.parse(this.responseText);
       var rate = data[6];
       showRates(rate, lastRate);
@@ -24,32 +24,29 @@ function main () {
     var oReq = new XMLHttpRequest();
     oReq.onload = reqListener;
     oReq.onerror = reqError;
-    oReq.open('get', urlBitfinex, true);
+    oReq.open('get', url, true);
     oReq.send();
   }, updateFrequencyInMs);
 
-  function reqError(err) {
-  console.log('Fetch Error: ', err);
-}
+  function reqError (err) {
+    console.log('Fetch Error: ', err);
+  }
 
-  function showRates(rate, lastRate) {
+  function showRates (rate, lastRate) {
     var roundedRate = rate.toFixed(2);
 
     if (rate === lastRate) {
-      formatRate('.bitfinex-rate', '#949494', roundedRate);
-    }
-    else if (rate > lastRate) {
-      formatRate('.bitfinex-rate', '#8bc040', roundedRate);
+      formatRate('.rate', '#949494', roundedRate);
+    } else if (rate > lastRate) {
+      formatRate('.rate', '#8bc040', roundedRate);
     } else if (rate < lastRate) {
-      formatRate('.bitfinex-rate', '#de5f66', roundedRate);
+      formatRate('.rate', '#de5f66', roundedRate);
     }
   }
 
-  function formatRate(element, color, roundedRate) {
+  function formatRate (element, color, roundedRate) {
     document.querySelector(element).style.color = color;
     document.querySelector(element).innerHTML = roundedRate;
     document.querySelector(element).style.color = color;
   }
-}
-
-main();
+})();
